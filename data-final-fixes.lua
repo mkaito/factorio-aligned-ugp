65634010f8b4d24eca6933f6b965f212a1cc600d
Michishige Kaito <me@mkaito.com>
2018-04-25 22:39:23 +0100

    Turns out that Lua is insane

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,1 +1,1 @@
-data.raw["pipe-to-ground"]["pipe-to-ground"].fluid_box.pipe_connections[1].max_underground_distance = 11
+data.raw["pipe-to-ground"]["pipe-to-ground"].fluid_box.pipe_connections[2].max_underground_distance = 11
